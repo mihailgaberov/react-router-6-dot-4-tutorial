bd51e70e6eea60153627fee83e062c2ee205ca85
mihail.gaberov <mihail.gaberov@ysura.com>
2022-09-26 13:26:20 +0200

    👨🏻‍🎓 URL Prams in Loaders. Updating data

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
-import Root, { loader as rootLoader } from "./routes/root";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from "./routes/root";
 
+import EditContact from "./routes/edit";
 import ErrorPage from "./error-page";
 import "./index.css";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -13,10 +17,17 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
       },
     ],
   },
--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,4 +1,9 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
+
+export async function loader({ params }) {
+  return getContact(params.contactId);
+}
 
 export default function Contact() {
   const contact = {
